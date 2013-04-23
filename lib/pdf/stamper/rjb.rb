4db77d42c9ee9ba7e933f8dafc20c9c6a8f049cb
Cory Preus <cory@softwareforgood.com>
2013-04-23 13:49:49 -0500

    Fix missing param on #to_s for RJB.

--- lib/pdf/stamper/rjb.rb
+++ lib/pdf/stamper/rjb.rb
@@ -70,8 +70,8 @@ module PDF
     
     # Takes the PDF output and sends as a string.  Basically its sole
     # purpose is to be used with send_data in rails.
-    def to_s
-      fill
+    def to_s(flatten_form=true)
+      fill(flatten_form)
       @baos.toByteArray
     end
     
